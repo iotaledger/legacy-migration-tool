c954506a62451e9e4de767519cfb5c8527f5ee54
evavirseda <evirseda@boxfish.studio>
2023-10-23 17:59:06 +0200

    feat: update congratulations view

--- packages/shared/routes/setup/Congratulations.svelte
+++ packages/shared/routes/setup/Congratulations.svelte
@@ -1,24 +1,12 @@
 <script lang="typescript">
-    import { get } from 'svelte/store'
     import { onDestroy, onMount } from 'svelte'
     import { Animation, Button, Icon, OnboardingLayout, Text } from 'shared/components'
-    import { mobile } from 'shared/lib/app'
-    import { convertToFiat, currencies, exchangeRates, formatCurrency } from 'shared/lib/currency'
     import { Platform } from 'shared/lib/platform'
     import { promptUserToConnectLedger } from 'shared/lib/ledger'
-    import {
-        LOG_FILE_NAME,
-        migration,
-        migrationLog,
-        resetMigrationState,
-        totalMigratedBalance,
-    } from 'shared/lib/migration'
-    import { activeProfile, newProfile, saveProfile, setActiveProfile, updateProfile } from 'shared/lib/profile'
+    import { LOG_FILE_NAME, migration, migrationLog, resetMigrationState } from 'shared/lib/migration'
+    import { activeProfile } from 'shared/lib/profile'
     import { appRouter, ledgerRouter } from '@core/router'
-    import { LedgerAppName } from 'shared/lib/typings/ledger'
-    import { formatUnitBestMatch } from 'shared/lib/units'
     import { getProfileDataPath, walletSetupType } from 'shared/lib/wallet'
-    import { AvailableExchangeRates, CurrencyTypes } from 'shared/lib/typings/currency'
     import { Locale } from '@core/i18n'
     import { SetupType } from 'shared/lib/typings/setup'
 
@@ -28,43 +16,18 @@
 
     const wasMigrated = $didComplete
 
-    let localizedBody = 'body'
-    let localizedValues = {}
     let logExported = false
+    let isNotALedgerProfile: boolean = false
 
     onMount(() => {
         if (!wasMigrated) {
-            if ($walletSetupType === SetupType.FireflyLedger) {
-                localizedBody = 'fireflyLedgerBody'
-            }
-            // This is the last screen in onboarding for all flows i.e., if you create a new wallet or import stronghold
-            // When this component mounts, ensure that the profile is persisted in the local storage.
-            $newProfile = { ...$newProfile, name: $newProfile.id }
-            saveProfile($newProfile)
-            setActiveProfile($newProfile.id)
-
-            newProfile.set(null)
-        } else {
-            if ($walletSetupType === SetupType.TrinityLedger) {
-                localizedBody = 'trinityLedgerBody'
-                localizedValues = { legacy: LedgerAppName.IOTALegacy }
-
-                updateProfile('ledgerMigrationCount', $activeProfile.ledgerMigrationCount + 1)
+            if ($walletSetupType === SetupType.FireflyLedger || $walletSetupType === SetupType.TrinityLedger) {
+                isNotALedgerProfile = false
             } else {
-                localizedBody = 'softwareMigratedBody'
+                isNotALedgerProfile = true
             }
         }
     })
-
-    const fiatbalance = formatCurrency(
-        convertToFiat(
-            // Only show actually migrated balance to user
-            $totalMigratedBalance,
-            get(currencies)?.[CurrencyTypes.USD],
-            get(exchangeRates)?.[AvailableExchangeRates.USD]
-        ),
-        AvailableExchangeRates.USD
-    )
 
     const handleContinueClick = (): void => {
         if (wasMigrated) {
@@ -117,36 +80,28 @@
 
 <OnboardingLayout allowBack={false}>
     <div slot="leftpane__content">
-        {#if wasMigrated}
-            <div class="relative flex flex-col items-center bg-gray-100 dark:bg-gray-900 rounded-2xl mt-10 p-10 pb-6">
-                <div class="bg-green-500 rounded-2xl absolute -top-6 w-12 h-12 flex items-center justify-center">
-                    <Icon icon="success-check" classes="text-white" />
-                </div>
-                <Text type="h2" classes="mb-6 text-center">{locale('views.congratulations.fundsMigrated')}</Text>
-                <Text type="p" secondary classes="mb-6 text-center">
-                    {locale(`views.congratulations.${localizedBody}`, { values: localizedValues })}
-                </Text>
-                <Text type="h2">{formatUnitBestMatch($totalMigratedBalance, true, 3)}</Text>
-                <Text type="p" highlighted classes="py-1 uppercase">{fiatbalance}</Text>
+        <div class="relative flex flex-col items-center bg-gray-100 dark:bg-gray-900 rounded-2xl mt-10 p-10 pb-6">
+            <div class="bg-green-500 rounded-2xl absolute -top-6 w-12 h-12 flex items-center justify-center">
+                <Icon icon="success-check" classes="text-white" />
             </div>
-        {:else}
-            <div class="relative flex flex-col items-center bg-gray-100 dark:bg-gray-900 rounded-2xl mt-10 p-10 pb-6">
-                <div class="bg-green-500 rounded-2xl absolute -top-6 w-12 h-12 flex items-center justify-center">
-                    <Icon icon="success-check" classes="text-white" />
-                </div>
-                <Text type="h2" classes="mb-5 text-center">{locale('views.congratulations.title')}</Text>
-                <Text type="p" secondary classes="mb-2 text-center"
-                    >{locale(`views.congratulations.${localizedBody}`)}</Text
-                >
-            </div>
-        {/if}
+            <Text type="h2" classes="mb-5 text-center">{locale('views.congratulations.title')}</Text>
+            <Text type="p" secondary classes="mb-2 text-center">{locale('views.congratulations.body')}</Text>
+        </div>
     </div>
-    <div slot="leftpane__action">
+    <div slot="leftpane__action" class="flex flex-col space-y-4">
+        <Button icon="profile" classes="w-full" secondary>
+            {locale('views.congratulations.migrateAnotherProfile')}
+        </Button>
+        {#if isNotALedgerProfile}
+            <Button icon="export" classes="w-full" secondary>
+                {locale('views.congratulations.exportStronghold')}
+            </Button>
+        {/if}
         <Button classes="w-full" onClick={() => handleContinueClick()}>
             {locale(`${wasMigrated && !logExported ? 'views.congratulations.exportMigration' : 'actions.finishSetup'}`)}
         </Button>
     </div>
-    <div slot="rightpane" class="w-full h-full flex justify-center {!$mobile && 'bg-pastel-yellow dark:bg-gray-900'}">
+    <div slot="rightpane" class="w-full h-full flex justify-center bg-pastel-yellow dark:bg-gray-900">
         <Animation classes="setup-anim-aspect-ratio" animation="congratulations-desktop" />
     </div>
 </OnboardingLayout>
